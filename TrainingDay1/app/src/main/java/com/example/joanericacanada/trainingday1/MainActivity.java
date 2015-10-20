8201a31cc2ff6256d3c82618fdb262ce0e5fc0af
JoanEricaCanada <joanerica.canada@gmail.com>
2015-10-20 08:16:22 +0800

    added ViewHolder in MainActivity.java

--- TrainingDay1/app/src/main/java/com/example/joanericacanada/trainingday1/MainActivity.java
+++ TrainingDay1/app/src/main/java/com/example/joanericacanada/trainingday1/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.joanericacanada.trainingday1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ViewHolder hold = new ViewHolder();
+        hold.editTxt = (EditText)findViewById(R.id.editText);
+        hold.txtView = (TextView)findViewById(R.id.textView2);
+        hold.button = (Button)findViewById(R.id.button9);
+        hold.chkBox = (CheckBox)findViewById(R.id.checkBox2);
+    }
+
+    static class ViewHolder{
+        EditText editTxt;
+        TextView txtView;
+        Button button;
+        CheckBox chkBox;
     }
 }
