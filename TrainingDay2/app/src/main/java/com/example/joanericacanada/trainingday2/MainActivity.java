da85499a9ceda2879548480c14c741f89451dbea
JoanEricaCanada <joanerica.canada@gmail.com>
2015-10-20 11:24:21 +0800

    implemented OnClickListener

--- TrainingDay2/app/src/main/java/com/example/joanericacanada/trainingday2/MainActivity.java
+++ TrainingDay2/app/src/main/java/com/example/joanericacanada/trainingday2/MainActivity.java
@@ -2,38 +2,46 @@ package com.example.joanericacanada.trainingday2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
     TextView txtViewOut;
     Button btnOK;
     Button btnCancel;
+
+    private static final String TAG = "myLogs";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(TAG, "find view elements");
         txtViewOut = (TextView)findViewById(R.id.tvOut);
-
-
         btnOK = (Button)findViewById(R.id.btnOk);
-        btnOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txtViewOut.setText("Button Ok clicked");
-            }
-        });
-
-
         btnCancel = (Button)findViewById(R.id.btnCancel);
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txtViewOut.setText("Button Cancel clicked");
-            }
-        });
+
+        Log.d(TAG, "assign listener to buttons");
+        btnOK.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "define the button that invoked the listener by id");
+        switch (v.getId()) {
+            case R.id.btnOk:
+                Log.d(TAG, "OK button");
+                txtViewOut.setText("OK button was clicked");
+                break;
+            case R.id.btnCancel:
+                Log.d(TAG, "Cancel button");
+                txtViewOut.setText("Cancel button was clicked");
+                break;
+        }
     }
 }
