62b80067a69a35dd4ea2fb29862b539b66d748e0
JoanEricaCanada <joanerica.canada@gmail.com>
2015-10-21 10:35:19 +0800

    setting extras and getting activity results

---
+++ TrainingDay3/app/src/main/java/com/example/joanericacanada/trainingday3/NameActivity.java
@@ -0,0 +1,35 @@
+package com.example.joanericacanada.trainingday3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by joanericacanada on 10/21/15.
+ */
+public class NameActivity extends Activity implements View.OnClickListener {
+
+    EditText edtTxtName;
+    Button btnOk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.name);
+
+        edtTxtName = (EditText)findViewById(R.id.etName);
+        btnOk = (Button)findViewById(R.id.btnOK);
+        btnOk.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        Intent intent = new Intent();
+        intent.putExtra("name", edtTxtName.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
